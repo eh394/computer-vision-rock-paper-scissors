fc7b47cf949807de37997a32d6dd6f20c593f71a
eh394 <hazla.ewa@gmail.com>
2022-12-18 22:21:31 +0000

    countdown added

--- camera_rps.py
+++ camera_rps.py
@@ -1,11 +1,14 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+import time
 model = load_model('keras_model.h5')
 
 labels = open('labels.txt', 'r').read().split('\n')
 labels = [entry[2:] for entry in labels]
 labels.pop()
+
+start = time.time()
 
 
 def get_prediction():
@@ -14,25 +17,29 @@ def get_prediction():
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
     while True: 
-        # capture frame by frame
-        ret, frame = cap.read()
-        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-        image_np = np.array(resized_frame)
-        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-        flipped_image = cv2.flip(normalized_image, 1)
-        data[0] = flipped_image
-        prediction = model.predict(data)
-        # display image in a window, the arguments stand for 'frame': a string representing the name of the window in which image is to be displayed; frame: the image that is to be displayed
-        cv2.imshow('frame', frame)
-        # # Press q to close the window
+        if time.time() < start + 5:
+            # capture frame by frame
+            ret, frame = cap.read()
+            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+            image_np = np.array(resized_frame)
+            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+            flipped_image = cv2.flip(normalized_image, 1)
+            data[0] = flipped_image
+            prediction = model.predict(data)
+            # display image in a window, the arguments stand for 'frame': a string representing the name of the window in which image is to be displayed; frame: the image that is to be displayed
+            cv2.imshow('frame', frame)
+            # # Press q to close the window
 
-        for i in range(len(labels)):
-            if np.argmax(prediction) == i:
-                prediction = labels[i]
-                print(prediction)
-        
+            # if time.time() > start + 5:      
+            for i in range(len(labels)):
+                if np.argmax(prediction) == i:
+                    prediction = labels[i]
+                    break
+                           
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        
+    print(f' You chose {prediction}.')
                 
     # After the loop release the cap object
     cap.release()
