6936fd67b74ab99345b1a2af8fdb3d9d0750d9c9
eh394 <hazla.ewa@gmail.com>
2022-12-18 15:26:15 +0000

    Simpler code for manual rock, paper, scissors uploaded.

--- manual_rps.py
+++ manual_rps.py
@@ -1,6 +1,5 @@
 import random
 
-choices = ['rock', 'paper', 'scissors']
 
 def get_computer_choice():
     choices = ['Rock', 'Paper', 'Scissors']
@@ -9,17 +8,33 @@ def get_computer_choice():
     return computer_choice
 
 def get_user_choice():
-    while True:
-        user_choice = input('Please enter rock, paper or scissors: ')
-        if user_choice.lower() not in choices:
-            print('Invalid entry. Please enter rock, paper, or scissors.')
-        else:
-            user_choice = user_choice.lower()
-            print(user_choice)
-            break    
+    user_choice = input('Please enter Rock, Paper or Scissors: ')
+    print(user_choice)
     return user_choice
 
 get_computer_choice()
 get_user_choice()
 
 
+# import random
+
+# choices = ['rock', 'paper', 'scissors']
+
+# def get_computer_choice():
+#     computer_choice = random.choice(choices)
+#     print(computer_choice)
+#     return computer_choice
+
+# def get_user_choice():
+#     while True:
+#         user_choice = input('Please enter rock, paper or scissors: ')
+#         if user_choice.lower() not in choices:
+#             print('Invalid entry. Please enter rock, paper, or scissors.')
+#         else:
+#             user_choice = user_choice.lower()
+#             print(user_choice)
+#             break    
+#     return user_choice
+
+# get_computer_choice()
+# get_user_choice()
