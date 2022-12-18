a4bcda4711cdb3ce549f2ad6c017a981792dfe29
eh394 <hazla.ewa@gmail.com>
2022-12-18 15:12:40 +0000

    Initial code for manual rock, paper, scissors game added.

---
+++ manual_rps.py
@@ -0,0 +1,24 @@
+import random
+
+choices = ['rock', 'paper', 'scissors']
+
+def get_computer_choice():
+    computer_choice = random.choice(choices)
+    print(computer_choice)
+    return computer_choice
+
+def get_user_choice():
+    while True:
+        user_choice = input('Please enter rock, paper or scissors: ')
+        if user_choice.lower() not in choices:
+            print('Invalid entry. Please enter rock, paper, or scissors.')
+        else:
+            user_choice = user_choice.lower()
+            print(user_choice)
+            break    
+    return user_choice
+
+get_computer_choice()
+get_user_choice()
+
+
